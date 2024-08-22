8f4ae168ea8b3dc0d2d3982444107adec904fe33
alazeprt <nono135246@126.com>
2024-08-22 18:29:20 +0800

    Fix bugs

--- src/main/java/com/alazeprt/serverstore/papi/ServerStoreExpansion.java
+++ src/main/java/com/alazeprt/serverstore/papi/ServerStoreExpansion.java
@@ -43,19 +43,26 @@ public class ServerStoreExpansion extends PlaceholderExpansion {
             return store.getString("sell." + project + ".player_limit");
         } else if (params.startsWith("buy_") && params.endsWith("_total_limit") && store.contains("buy." + params.substring(4, params.length() - 12))) {
             String project = params.substring(4, params.length() - 12);
-            return store.getString("buy." + project + ".total_limit");
+            return store.getString("buy." + project + ".total_limit") == null ?
+                    "无限" : store.getString("buy." + project + ".total_limit");
         } else if (params.startsWith("sell_") && params.endsWith("_total_limit") && store.contains("sell." + params.substring(5, params.length() - 12))) {
             String project = params.substring(5, params.length() - 12);
-            return store.getString("sell." + project + ".total_limit");
+            return store.getString("sell." + project + ".total_limit") == null ?
+                    "无限" : store.getString("sell." + project + ".total_limit");
         } else if (params.startsWith("buy_") && params.endsWith("_player_buy") && store.contains("buy." + params.substring(4, params.length() - 11))) {
             String project = params.substring(4, params.length() - 11);
-            return data.getString("buy." + project + ".players." + player.getName());
+            return data.getString("buy." + project + ".players." + player.getName()) == null ?
+                    "0" : data.getString("buy." + project + ".players." + player.getName());
         } else if (params.startsWith("sell_") && params.endsWith("_player_sell") && store.contains("sell." + params.substring(5, params.length() - 12))) {
             String project = params.substring(5, params.length() - 12);
-            return data.getString("sell." + project + ".players." + player.getName());
+            return data.getString("sell." + project + ".players." + player.getName()) == null ?
+                    "0" : data.getString("sell." + project + ".players." + player.getName());
         } else if (params.startsWith("buy_") && params.endsWith("_total_buy") && store.contains("buy." + params.substring(4, params.length() - 10))) {
             String project = params.substring(4, params.length() - 10);
             BigDecimal bigDecimal = new BigDecimal(0);
+            if(data.getConfigurationSection("buy." + project + ".players") == null) {
+                return "0";
+            }
             for(String key : data.getConfigurationSection("buy." + project + ".players").getKeys(false)) {
                 bigDecimal = bigDecimal.add(new BigDecimal(data.getString("buy." + project + ".players." + key)));
             }
@@ -63,6 +70,9 @@ public class ServerStoreExpansion extends PlaceholderExpansion {
         } else if (params.startsWith("sell_") && params.endsWith("_total_sell") && store.contains("sell." + params.substring(5, params.length() - 11))) {
             String project = params.substring(5, params.length() - 11);
             BigDecimal bigDecimal = new BigDecimal(0);
+            if(data.getConfigurationSection("sell." + project + ".players") == null) {
+                return "0";
+            }
             for(String key : data.getConfigurationSection("sell." + project + ".players").getKeys(false)) {
                 bigDecimal = bigDecimal.add(new BigDecimal(data.getString("sell." + project + ".players." + key)));
             }
